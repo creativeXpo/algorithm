bf032a83358e1bb3c0c1f0cae4c681dcc22be451
Golam Kibria <soysob87@gmail.com>
2025-01-27 11:09:28 +0600

    Update prime-number.php

--- prime-number.php
+++ prime-number.php
@@ -2,22 +2,17 @@
 
 // Write a program to print all prime numbers between 1 and 20.
 
-for($num = 2; $num <= 20; $num++){
-	
+for ($num = 2; $num <= 20; $num++) {
     $isPrime = true;
-    
-    for($i = 2; $i <= sqrt($num); $i++){
-    
-    	if($num % $i === 0){
-        	$isPrime = false;
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i == 0) {
+            $isPrime = false;
             break;
         }
     }
-    
-    if($isPrime){
-    	echo $num . " ";
+    if ($isPrime) {
+        echo $num . " ";
     }
-    
 }
 
 // Write a program to check if a number is prime.
